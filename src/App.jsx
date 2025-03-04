81500b74325a48171cdc725789af9145fceaa632
SurekaP1 <psureka@std.foc.sab.ac.lk>
2025-03-04 09:58:42 -0800

    Updated Title component and related files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
-
-
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
-      <Programs/> 
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer' />
+     <Programs/>
+     </div>
     </div>
   )
 }
