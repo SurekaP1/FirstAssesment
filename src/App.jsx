af442390411e20b01ee2ae04f0c717441d37162c
SurekaP1 <psureka@std.foc.sab.ac.lk>
2025-03-05 00:50:51 -0800

    Updated files and added intracked files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
+import { Campus } from './components/Campus/Campus'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
      <Programs/>
      <About/>
      <Title subTitle='Gallery' title='Campus Photos' />
+     <Campus/>
      </div>
     </div>
   )
